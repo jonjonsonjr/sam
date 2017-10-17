940d66f12fe83d183c9e8321d23eadaee99b33aa
Jon Johnson <jonjonsonjr@gmail.com>
2017-10-16 20:37:27 -0700

    Add index.html

--- index.js
+++ index.js
@@ -1,62 +1,14 @@
-const playlists = [
-  'https://open.spotify.com/user/1144330891/playlist/3TwtYYOux2ogFw7C970YcI',
-  'https://open.spotify.com/user/1155899437/playlist/2XFGVQwJ0D9uvSImoOrC9w',
-  'https://open.spotify.com/user/1155899437/playlist/6qaUi2aj8Wtwkrrmzvqdmd',
-  'https://open.spotify.com/user/1281672208/playlist/7iovz3ZXq7xypgK5zkkprc',
-  'https://open.spotify.com/user/22r22vrehfvv2pa54va2mn5ya/playlist/1OOqD9kQCal0C1g90Rfqai',
-  'https://open.spotify.com/user/angel--zero--/playlist/6Rw9xsZlycCcWcLVVTH7tr',
-  'https://open.spotify.com/user/carter.santos/playlist/5bXZLtnfqq2UbTEcZaHbqF'
-];
+function render_task(schedule, index) {
+  const time = Date.now(); // TODO
+  const task_id = 1 + (index * 2); // TODO
+  const {playlist, name} = schedule;
 
-const tuples = playlists.map(p => {
-  const matches = p.match('https://open.spotify.com/user/(.*)/playlist/(.*)');
-  return {
-    user_id: matches[1],
-    playlist_id: matches[2]
-  };
-});
-
-const schedules = tuples.map((t, index) => {
-  let {user_id, playlist_id} = t;
-  return {
-    hour: 23,
-    minute: index,
-    playlist: `spotify:user:${user_id}:playlist:${playlist_id}:play`
-  };
-});
-
-console.log(print_xml(schedules));
-
-function print_xml(schedules) {
-  let profiles_xml = schedules.map((schedule, index) => {
-    const id = (index * 2) + 2;
-    const time = Date.now();
-    const task_id = 1 + (index * 2);
-    const {hour, minute} = schedule;
-    return `
-  <Profile sr="prof${id}" ve="2">
-    <cdate>${time}</cdate>
-    <edate>${time + 1}</edate>
-    <id>${id}</id>
-    <mid0>${task_id}</mid0>
-    <Time sr="con0">
-      <fh>${hour}</fh>
-      <fm>${minute}</fm>
-      <th>-1</th>
-      <tm>-1</tm>
-    </Time>
-  </Profile>`;
-  });
-  let tasks_xml = schedules.map((schedule, index) => {
-    const time = Date.now(); // TODO
-    const task_id = 1 + (index * 2); // TODO
-    const {playlist} = schedule;
-    return `
+  return `
   <Task sr="task${task_id}">
     <cdate>${time}</cdate>
     <edate>${time + 1}</edate>
     <id>${task_id}</id>
-    <nme>${playlist}</nme>
+    <nme>${name}</nme>
     <pri>100</pri>
     <Action sr="act0" ve="7">
       <code>20</code>
@@ -112,11 +64,33 @@ function print_xml(schedules) {
       <Int sr="arg9" val="0"/>
     </Action>
   </Task>`;
-  });
+}
+
+function render_profile(schedule, index) {
+  const id = (index * 2) + 2;
   const time = Date.now();
-  let profiles = profiles_xml.map((p, i) => (i * 2) + 2);
-  let tasks = tasks_xml.map((p, i) => 1 + (i * 2));
-  let projects_xml = `
+  const task_id = 1 + (index * 2);
+  const {hour, minute} = schedule;
+
+  return `
+  <Profile sr="prof${id}" ve="2">
+    <cdate>${time}</cdate>
+    <edate>${time + 1}</edate>
+    <id>${id}</id>
+    <mid0>${task_id}</mid0>
+    <Time sr="con0">
+      <fh>${hour}</fh>
+      <fm>${minute}</fm>
+      <th>-1</th>
+      <tm>-1</tm>
+    </Time>
+  </Profile>`;
+}
+
+function render_project(profiles, tasks) {
+  const time = Date.now();
+
+  return `
   <Project sr="proj0" ve="2">
     <cdate>${time}</cdate>
     <mdate>${time + 1}</mdate>
@@ -127,7 +101,17 @@ function print_xml(schedules) {
       <nme>mw_action_home</nme>
     </Img>
   </Project>`;
+}
 
-  return `<TaskerData sr="" dvi="1" tv="5.0u6m">${profiles_xml.join('')}${projects_xml}${tasks_xml.join('')}
+function print_xml(schedules) {
+  let profiles_xml = schedules.map(render_profile);
+  let tasks_xml = schedules.map(render_task);
+
+  let profiles = profiles_xml.map((p, i) => (i * 2) + 2);
+  let tasks = tasks_xml.map((p, i) => 1 + (i * 2));
+
+  let project_xml = render_project(profiles, tasks);
+
+  return `<TaskerData sr="" dvi="1" tv="5.0u6m">${profiles_xml.join('')}${project_xml}${tasks_xml.join('')}
 </TaskerData>`;
-};
+}
